b28da740a262b0119811ab6d3da935b9bfe3482c
Hericlys Luiz T. de Sá <hericlysdesa@gmail.com>
2024-10-21 16:31:22 -0300

    iniciando testes do site

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class PagesURLsTest(TestCase):
+    def test_pages_home_is_using_the_correct_url(self):
+        home_url = reverse('pages:home')
+        self.assertEqual(home_url, '/')
